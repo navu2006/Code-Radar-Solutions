8940b67bb035b5347dac9c7b371a27acb8661310
navu2006 <navdishaj06@gmail.com>
2025-02-05 19:52:03 +0530

    Solution to Perform Bitwise Right Shift

---
+++ Perform Bitwise Right Shift@@67882a731b4392d8fc1577e6/code.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2, result;
+
+    // Input two integers from the user
+    scanf("%d %d", &num1, &num2);
+
+    // Perform bitwise OR operation
+    result = num1 >> num2;
+
+    // Output only the result
+    printf("%d\n", result);
+
+    return 0;
+}
